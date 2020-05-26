875e6d3dc35f03d3b587b83aa139a20a7315bcfd
kmnrm <58434461+kmnrm@users.noreply.github.com>
2020-05-27 00:21:43 +0800

    Reduce the number of queries for post detail page

--- blog/views.py
+++ blog/views.py
@@ -47,7 +47,7 @@ def index(request):
 
 
 def post_detail(request, slug):
-    post = Post.objects.get(slug=slug)
+    post = Post.objects.select_related('author').get(slug=slug)
     comments = post.comments.prefetch_related('author')
     serialized_comments = []
     for comment in comments:
